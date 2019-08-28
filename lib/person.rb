3a7e366dd58d4737d08b5ceb9ce8032a8b37ef88
Adrian Samaniego <llathander@gmail.com>
2019-08-28 09:09:15 -0400

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,34 @@
+require 'pry'
+
+class Person
+    def initialize
+        @name = name
+        @job = job
+        
+    end
+
+    
+
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+    def job=(job)
+        @job = job
+    end
+
+    def job
+        @job
+    end
+
+    binding.pry
+
+end
+
+    
+
+
